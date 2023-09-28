2fc427c7278e32c6c69a015aa2b9ec282e7956c9
if-loop69420 <j.sztavi@pm.me>
2023-09-28 18:17:16 +0200

    Further working on the abstract. For now writing it in english, because i suck at german

---
+++ .auctex-auto/main.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "robo4you"
+    "ros-site"
+    "wombat-controller"
+    "Hope_Learning_TensorFlow"))
+ '(or :bibtex :latex))
+
