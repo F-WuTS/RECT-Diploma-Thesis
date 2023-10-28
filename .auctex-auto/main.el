1c2a4d83290ef776f36c8171643233d71a8e2251
if-loop69420 <j.sztavi@pm.me>
2023-10-28 14:02:01 +0200

    Started writing stuff in the technology section

--- .auctex-auto/main.el
+++ .auctex-auto/main.el
@@ -1,10 +1,24 @@
 (TeX-add-style-hook
  "main"
  (lambda ()
-   (LaTeX-add-bibitems
-    "robo4you"
-    "ros-site"
-    "wombat-controller"
-    "ipc-begriff"))
- '(or :bibtex :latex))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("book" "11pt" "a4paper" "openany")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "sections/Settings"
+    "sections/titlepage"
+    "sections/declarationof"
+    "sections/acknowledgement"
+    "sections/abstract"
+    "sections/introduction"
+    "sections/technology"
+    "sections/implementation"
+    "sections/tests"
+    "sections/conclusion"
+    "sections/messbox"
+    "book"
+    "bk11"
+    "titlesec")
+   (LaTeX-add-bibliographies))
+ :latex)
 
